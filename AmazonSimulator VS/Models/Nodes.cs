0c1c16bfa905f98d45bd2863bc6fb3015882a32e
kevinhaakma <haak1701@student.nhl.nl>
2018-09-18 13:10:26 +0200

    Nodes.cs

---
+++ AmazonSimulator VS/Models/Nodes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Nodes
+    {
+        public double x;
+        public double y;
+        public double z;
+
+        private Shelf shelf;
+
+        public Shelf GetShelf()
+        {
+            Shelf tmp = shelf;
+            shelf = null;
+            return tmp;
+        }
+
+        public void SetShelf(Shelf shelf)
+        {
+            this.shelf = shelf;
+            this.shelf.Move(x, y, z);
+        }
+    }
+}
