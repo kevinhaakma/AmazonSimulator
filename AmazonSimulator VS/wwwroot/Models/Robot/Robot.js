c89cfd0f786fdb6220c6968a8e46811da0eafa3f
Richfart <jong1737@student.nhl.nl>
2018-09-26 12:21:06 +0200

    leuke dingen hoor

---
+++ AmazonSimulator VS/wwwroot/Models/Robot/Robot.js
@@ -0,0 +1,29 @@
+﻿class Robot extends THREE.Group {
+    constructor() {
+        super();
+
+        this.init();
+    }
+
+    init() {
+
+        var selfRef = this;
+
+        var geometry = new THREE.BoxGeometry(0.9, 0.3, 0.9);
+        var robotmaterials = [
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("Models/Robot/robot_side.png"), side: THREE.DoubleSide }), //LEFT
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("Models/Robot/robot_side.png"), side: THREE.DoubleSide }), //RIGHT
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("Models/Robot/robot_top.png"), side: THREE.DoubleSide }), //TOP
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("Models/Robot/robot_bottom.png"), side: THREE.DoubleSide }), //BOTTOM
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("Models/Robot/robot_front.png"), side: THREE.DoubleSide }), //FRONT
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("Models/Robot/robot_front.png"), side: THREE.DoubleSide }), //BACK
+        ];
+        var robot = new THREE.Mesh(geometry, robotmaterials);
+        robot.position.y = 0.15;
+        robot.name = "robot";
+        robot.userData = { status: "idle" };
+
+        var group = new THREE.Group();
+        selfRef.add(robot);
+    }
+}
---
+++ AmazonSimulator VS/wwwroot/Models/Shelf/Shelf.js
@@ -0,0 +1,27 @@
+﻿class Shelf extends THREE.Group {
+    constructor() {
+        super();
+
+        this.init();
+    }
+
+    init() {
+
+        var selfRef = this;
+
+        var geometry = new THREE.BoxGeometry(0.7, 1.4, 0.7);
+        var shelfmaterials = [
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("Models/Shelf/ShelfSide.png"), side: THREE.DoubleSide }), //LEFT
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("Models/Shelf/ShelfSide.png"), side: THREE.DoubleSide }), //RIGHT
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("Models/Shelf/ShelfBotTop.png"), side: THREE.DoubleSide }), //TOP
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("Models/Shelf/ShelfBotTop.png"), side: THREE.DoubleSide }), //BOTTOM
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("Models/Shelf/ShelfSide.png"), side: THREE.DoubleSide }), //FRONT
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("Models/Shelf/ShelfSide.png"), side: THREE.DoubleSide }), //BACK
+        ];
+        var shelf = new THREE.Mesh(geometry, shelfmaterials);
+        shelf.position.y = 0.7;
+
+        var group = new THREE.Group();
+        selfRef.add(shelf);
+    }
+}
