1e3b560e13b1848e9ecda643341c12db75c960f9
kevinhaakma <haak1701@student.nhl.nl>
2018-10-03 00:26:59 +0200

    Lighthouse en lightdinkjes

---
+++ AmazonSimulator VS/wwwroot/models/Lighthouse/Lighthouse.js
@@ -0,0 +1,41 @@
+﻿class Lighthouse extends THREE.Group {
+    constructor() {
+        super();
+
+        this.init();
+    }
+
+    init() {
+
+        var selfRef = this;
+
+        var mtlLoader = new THREE.MTLLoader();
+        mtlLoader.setPath('models/Lighthouse/');
+        var url = "lighthouse.mtl";
+        mtlLoader.load(url, function (materials) {
+
+            materials.preload();
+
+            var objLoader = new THREE.OBJLoader();
+            objLoader.setMaterials(materials);
+            objLoader.setPath('models/Lighthouse/');
+            objLoader.load('lighthouse.obj', function (object) {
+                var group = new THREE.Group();
+                object.scale.set(3, 3, 3);
+                object.rotation.y = Math.PI / 2;
+                object.position.set(27.5, 4.2, -6.75);
+                object.rotation.y = -Math.PI / 1.125;
+
+                group.add(object);
+
+                var light = new THREE.SpotLight(0xffffff, 1, 10);
+                light.position.set(27.85, 9.5, -6.9);
+                group.add(light);
+
+                selfRef.add(group);
+            });
+
+        });
+    }
+
+}
